02138a7fc5f59a9e93805669d4c39d5b2a409aa0
Stephen Sugden <me@stephensugden.com>
2018-12-11 18:03:06 +0100

    deploy: Limit concurrency of lambda preparation to 4
    Without this, you end up with numberOfLambdas concurrent npm processes, which open too many files and things break.

--- src/deploy/lambda-all/01-prep.js
+++ src/deploy/lambda-all/01-prep.js
@@ -1,11 +1,10 @@
-let parallel = require('run-parallel')
-let waterfall = require('run-waterfall')
+let parallel = require('run-parallel-limit')
 let prep = require('../lambda-one/prep')
 let _progress = require('../../util/progress')
 
 module.exports = function _prepare(params) {
   let {env, arc} = params
-  return function _prep(results, callback) {
+  return function _prep(results, finishedAll) {
 
     let total = results.length * 5 // 4 prep steps + 1 tick for bar instantiation
     let progress = _progress({
@@ -15,44 +14,38 @@ module.exports = function _prepare(params) {
     let tick = progress.tick
 
     let failedprep = []
-    waterfall([
-      function _goPrep(callback) {
-        parallel(results.map(pathToCode=> {
-          return function _prep(callback) {
-            prep({
-              env,
-              arc,
-              pathToCode,
-              tick,
-            },
-            function _prepped(err) {
-              if (err && err.message === 'cancel_not_found') {
-                failedprep.push(pathToCode)
-                tick('')
-                tick('')
-                tick('')
-                tick('')
-                tick('')
-                callback()
-              }
-              else if (err) {
-                callback(err)
-              }
-              else {
-                callback()
-              }
-            })
-          }
-        }), callback)
-      }
-    ],
-    function done(err) {
+    let tasks = results.map(pathToCode => finishedOne => {
+      prep({
+        env,
+        arc,
+        pathToCode,
+        tick,
+      },
+      function _prepped(err) {
+        if (err && err.message === 'cancel_not_found') {
+          failedprep.push(pathToCode)
+          tick('')
+          tick('')
+          tick('')
+          tick('')
+          tick('')
+          finishedOne()
+        }
+        else if (err) {
+          finishedOne(err)
+        }
+        else {
+          finishedOne()
+        }
+      })
+    })
+    parallel(tasks, 4, function done(err) {
       if (err) {
-        callback(err)
+        finishedAll(err)
       }
       else {
         let filtered = results.filter(pathToCode=> !failedprep.includes(pathToCode))
-        callback(null, filtered)
+        finishedAll(null, filtered)
       }
     })
   }
